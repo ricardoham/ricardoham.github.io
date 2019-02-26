72f743a8278bb6a7dfd921074e76368bdbb6761e
Ricardo T. Manoel Jr <rdomanoel@gmail.com>
2019-02-25 23:04:47 -0300

    Initial Commit

---
+++ src/pages/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        App Works!
+      </div>
+    )
+  }
+}
+
+export default App;
