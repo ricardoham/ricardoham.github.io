801bac4c83fabed08a6a868acd01a1f6afca6026
Ricardo T. Manoel Jr <rdomanoel@gmail.com>
2019-02-26 23:59:55 -0300

    letter spacing

--- src/components/header.js
+++ src/components/header.js
@@ -4,11 +4,11 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => (
   <div className="header">
     <nav className="header header__text">
-      <NavLink to="/">Profile</NavLink>
-      <NavLink to="/knowledge">Knowledge</NavLink>
-      <NavLink to="/resume">Resume</NavLink>
-      <NavLink to="/projects">Projects</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
+      <NavLink className="header header__text" to="/">Profile</NavLink>
+      <NavLink className="header header__text" to="/knowledge">Knowledge</NavLink>
+      <NavLink className="header header__text" to="/resume">Resume</NavLink>
+      <NavLink className="header header__text" to="/projects">Projects</NavLink>
+      <NavLink className="header header__text" to="/contact">Contact</NavLink>
     </nav>
   </div>
 )
