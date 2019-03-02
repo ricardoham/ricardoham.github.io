1e03591c2d4fa65a568c20f56920cf665033841c
Ricardo T. Manoel Jr <rdomanoel@gmail.com>
2019-03-02 00:47:19 -0300

    row on section of profile

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -5,12 +5,14 @@ class Profile extends React.PureComponent {
     return (
       <div className="container">
         <section>
+          <div className="row">
           <h2>Profile Context</h2>
-          <p>Front-End Web developer working with the most used and 
+          <p className="paragraph">Front-End Web developer working with the most used and 
             recent technologies: Modern JavaScript(ES6), React, Redux, 
             React- Native and so on. Always an enthusiast with JavaScript programming, 
             a geek passionate about expanding the knowledge, developing solutions 
             outside of the box to accelerate the delivery and quality of projects.</p>
+            </div>
         </section>
       </div>
     )
