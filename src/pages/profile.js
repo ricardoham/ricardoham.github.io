0c14b07a6298db827afbe6b8dca6b18a2bc12533
Ricardo T. Manoel Jr <rdomanoel@gmail.com>
2019-03-06 22:41:45 -0300

    profile picture rize with grid layout

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -4,20 +4,21 @@ import profile from '../assets/profile.jpg'
 class Profile extends React.PureComponent {
   render() {
     return (
-      <div className="container">
+      <div className="profile-container">
         <section>
           <div className="row">
-          <div class="col-3-of-4">
+          <div className="col-1-of-3">
 
           <img className="profile__image" alt="profile" src={profile} />
           </div>
-
+          <div className="col-2-of-3">
           <h2>Profile Context</h2>
           <p className="paragraph">Front-End Web developer working with the most used and 
             recent technologies: Modern JavaScript(ES6), React, Redux, 
             React- Native and so on. Always an enthusiast with JavaScript programming, 
             a geek passionate about expanding the knowledge, developing solutions 
             outside of the box to accelerate the delivery and quality of projects.</p>
+            </div>
             </div>
         </section>
       </div>
