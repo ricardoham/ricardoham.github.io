9a606743dea86f921c128211f09050e1f00f5a96
Ricardo T. Manoel Jr <rdomanoel@gmail.com>
2019-03-06 23:58:58 -0300

    more height to the header

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
-  <div>
-    <nav className="header">
-      <NavLink className="header__text" to="/">Profile</NavLink>
-      <NavLink className="header__text" to="/knowledge">Knowledge</NavLink>
-      <NavLink className="header__text" to="/resume">Resume</NavLink>
-      <NavLink className="header__text" to="/projects">Projects</NavLink>
-      <NavLink className="header__text" to="/contact">Contact</NavLink>
+  <div className="header">
+    <span>My Name</span>
+    <nav className="navbar">
+      <NavLink className="navbar__text" to="/">Profile</NavLink>
+      <NavLink className="navbar__text" to="/knowledge">Knowledge</NavLink>
+      <NavLink className="navbar__text" to="/resume">Resume</NavLink>
+      <NavLink className="navbar__text" to="/projects">Projects</NavLink>
+      <NavLink className="navbar__text" to="/contact">Contact</NavLink>
     </nav>
   </div>
 )
