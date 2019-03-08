7878e37aaafe0e6af01fef90a092264d202358e3
Ricardo T. Manoel Jr <rdomanoel@gmail.com>
2019-03-07 23:50:02 -0300

    template header with grid

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => (
   <div className="header">
     <span className="header-name">Ricardo T. Manoel Junior</span>
+    <span className="header-portfolio">Portfolio</span>
     <nav className="navbar">
       <NavLink className="navbar__text" to="/">Profile</NavLink>
       <NavLink className="navbar__text" to="/knowledge">Knowledge</NavLink>
