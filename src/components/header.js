d2b04b1b8accd17b39949f6ba5c4cb5ff4e01a70
Ricardo T. Manoel Jr <rdomanoel@gmail.com>
2019-03-11 23:34:17 -0300

    add fontawesome and list social icon

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 class Header extends React.PureComponent {
+
+  renderSocialIcons = () => {
+    return (
+    <>
+      <i className="fab fa-facebook-f"></i>
+      <i className="fab fa-linkedin-in"></i>
+      <i className="fab fa-github"></i>
+      <i className="fab fa-gitlab"></i>
+      <i className="fab fa-medium"></i>
+    </>
+    )
+  }
+
   render() {
     return (
       <div className="header-container">
         <span>Ricardo T. Manoel Junior</span>
         <span>Portfolio</span>
+        {this.renderSocialIcons()}
       </div>
     )
   }
