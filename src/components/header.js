44818ef16aaaaebac315e3631a0d315561b7be08
Ricardo T. Manoel Jr <rdomanoel@gmail.com>
2019-03-13 23:27:17 -0300

    define header to grid

--- src/components/header.js
+++ src/components/header.js
@@ -19,7 +19,7 @@ class Header extends React.PureComponent {
       <div className="header-container">
         <span className="header__text--name">Ricardo T. Manoel Junior</span>
         <span className="header__text--portfolio">Portfolio</span>
-        {this.renderSocialIcons()}
+        <span className="header__social-icons">{this.renderSocialIcons()}</span>
       </div>
     )
   }
