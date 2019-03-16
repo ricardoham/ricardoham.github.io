4a163ca652f88c563752f49d32d0cfc8de648ca5
Ricardo T. Manoel Jr <rdomanoel@gmail.com>
2019-03-15 22:59:22 -0300

    dev: header arrange the icons and header position

--- src/components/header.js
+++ src/components/header.js
@@ -6,22 +6,22 @@ class Header extends React.PureComponent {
   renderSocialIcons = () => {
     return (
     <>
-      <li>
+      <li className="header__social-icon">
         <a href="https://www.linkedin.com/in/ricardo-manoel">
           <i className="fab fa-linkedin-in"></i>
         </a>
       </li>
-      <li>
+      <li className="header__social-icon">
         <a href="https://github.com/ricardoham">
           <i className="fab fa-github"></i>
         </a>
       </li>
-      <li>
+      <li className="header__social-icon">
         <a href="https://gitlab.com/rmanoel">
           <i className="fab fa-gitlab"></i>
         </a>
       </li>
-      <li>
+      <li className="header__social-icon">
         <a href="https://medium.com/@rdomanoel">
         <i className="fab fa-medium"></i>
         </a>
@@ -35,7 +35,7 @@ class Header extends React.PureComponent {
       <div className="header-container">
         <span className="header__text--name">Ricardo T. Manoel Junior</span>
         <span className="header__text--portfolio">Portfolio</span>
-        <ul className="header__social-icons">{this.renderSocialIcons()}</ul>
+        <ul className="header__social-icons-list">{this.renderSocialIcons()}</ul>
         <Navbar />
       </div>
     )
