ee923f4a2baa8320b428938cc79dfb3c90531592
Ricardo Manoel <rdomanoel@gmail.com>
2021-03-03 14:53:16 -0300

    dev: spring parallax

--- src/components/parallax/Parallax.tsx
+++ src/components/parallax/Parallax.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { Parallax as SpringParallax, ParallaxLayer } from 'react-spring/renderprops-addons';
+import React, { useRef } from 'react';
+import { IParallax, Parallax, ParallaxLayer } from '@react-spring/parallax';
 
-const Parallax = ({ picture }) => {
+const SpringParallax = () => {
+  const springRef = useRef<IParallax>();
+
   return (
-    <SpringParallax pages={2}>
+    <Parallax ref={springRef} pages={3}>
+      <ParallaxLayer
+        offset={1}
+        speed={1}
+        style={{ backgroundColor: '#805E73' }}
+      />
+      <ParallaxLayer
+        offset={2}
+        speed={1}
+        style={{ backgroundColor: '#87BCDE' }}
+      />
+    </Parallax>
+  );
+};
 
-    </SpringParallax>
-    // <img
-    //   className="parallax"
-    //   src="assets/picture01.jpg"
-    // />
-  )
-}
-
-export default Parallax;
+export default SpringParallax;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,36 @@
-import Head from 'next/head'
-import Parallax from '../components/parallax/Parallax'
-import SpringSection from '../components/section/spring/SpringSection'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import SpringParallax from '../components/parallax/Parallax';
+import SpringSection from '../components/section/spring/SpringSection';
 
 export default function Home() {
   return (
     <div>
-      {/* <Parallax /> */}
-      {/* <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to my Portfolio!
-        </h1>
-
-        <p className={styles.description}>
-          My name is Ricardo Tadeu Manoel Junior,
-          I'm from Brazil I live on Campinas city, knows as Princesa do Oeste, the third most populous municipality in São Paulo state
-          And consider one of biggest center of technology and research in Latin America.
-        </p>
-
-        <p className={styles.description}>
-          I graduate from University of Campinas - Unicamp in 2016 with a degree in Analysis and Software Development.
-          In there I also performed a scientific study with the theme: Study of Failures on Virtual Machines in the Cloud Computing.
-        </p>
-        <span className={styles.footer}>Test</span>
-      </main> */}
-      <SpringSection>
-        <span>TEST</span>
-      </SpringSection>
+      <SpringParallax />
     </div>
-  )
+  );
 }
+
+// {/* <Head>
+//   <title>Create Next App</title>
+//   <link rel="icon" href="/favicon.ico" />
+// </Head>
+// <main className={styles.main}>
+//   <h1 className={styles.title}>
+//     Welcome to my Portfolio!
+//   </h1>
+
+//   <p className={styles.description}>
+//     My name is Ricardo Tadeu Manoel Junior,
+//     I'm from Brazil I live on Campinas city, knows as Princesa do Oeste, the third most populous municipality in São Paulo state
+//     And consider one of biggest center of technology and research in Latin America.
+//   </p>
+
+//   <p className={styles.description}>
+//     I graduate from University of Campinas - Unicamp in 2016 with a degree in Analysis and Software Development.
+//     In there I also performed a scientific study with the theme: Study of Failures on Virtual Machines in the Cloud Computing.
+//   </p>
+//   <span className={styles.footer}>Test</span>
+// </main> */}
+// <SpringSection>
+//   <span>TEST</span>
+// </SpringSection>
