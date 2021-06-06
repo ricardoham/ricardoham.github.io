0f41d6cd7e66b75ad9ba58efe810376038f30948
Ricardo Manoel <rdomanoel@gmail.com>
2021-06-05 21:53:17 -0300

    removing react-spring

--- src/components/section/spring/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import { animated } from 'react-spring';
-
-export const StyledArticle: any = styled(animated.article)`
-  padding: 12px;
-`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React, { useRef } from 'react';
 import Head from 'next/head';
-import { IParallax, Parallax, ParallaxLayer } from '@react-spring/parallax';
 import Banner from 'components/banner/Banner';
 import Profile from 'components/pages-content/profile/Profile';
 import Graduation from 'components/pages-content/graduation/Graduation';
@@ -11,8 +10,6 @@ import Footer from 'components/footer/Footer';
 import { prefix } from 'utils/prefix';
 
 export default function Home() {
-  const parallaxRef = useRef<IParallax>();
-
   return (
     <>
       <Head>
@@ -24,38 +21,13 @@ export default function Home() {
         />
       </Head>
       <div>
-        <Parallax
-          ref={parallaxRef}
-          pages={9}
-          style={{
-            backgroundImage: `url(${prefix}/assets/picture04.jpg)`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-          }}
-        >
-          <ParallaxLayer offset={0} speed={0} />
-          <ParallaxLayer offset={0} speed={1}>
             <Banner />
-          </ParallaxLayer>
-          <ParallaxLayer offset={1} speed={0.3}>
             <Profile />
-          </ParallaxLayer>
-          <ParallaxLayer offset={2.18} speed={0.3}>
             <Graduation />
-          </ParallaxLayer>
-          <ParallaxLayer offset={3} speed={0.3}>
             <WorkExp />
-          </ParallaxLayer>
-          <ParallaxLayer offset={4.3} speed={0.3}>
             <Skills />
-          </ParallaxLayer>
-          <ParallaxLayer offset={6.4} speed={0.3}>
             <Projects />
-          </ParallaxLayer>
-          <ParallaxLayer offset={8.5} speed={0.5}>
             <Footer />
-          </ParallaxLayer>
-        </Parallax>
       </div>
     </>
   );
