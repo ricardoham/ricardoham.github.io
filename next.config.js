c04886517fe8ee144672ad767561eaba7e7eedc7
Ricardo Manoel <rdomanoel@gmail.com>
2021-06-06 17:50:08 -0300

    fix: next build

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
-module.exports = {
-  webpack: (config) => {
-    config.module.rules.push(
-      {
-        test: /react-spring/,
-        sideEffects: true,
-      },
-      {
-        test: /\@react-spring\/parallax/,
-        sideEffects: true,
-      },
-    );
+// module.exports = {
+//   webpack: (config) => {
+//     config.module.rules.push(
+//       {
+//         test: /react-spring/,
+//         sideEffects: true,
+//       },
+//       {
+//         test: /\@react-spring\/parallax/,
+//         sideEffects: true,
+//       },
+//     );
 
-    return config;
-  },
-};
+//     return config;
+//   },
+// };
 
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
