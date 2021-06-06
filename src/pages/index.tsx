640ea90346d2cea1e6591ecefdf8030119ccfcf3
Ricardo Manoel <rdomanoel@gmail.com>
2021-06-05 22:19:36 -0300

    fix: next build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import Head from 'next/head';
+import { IParallax, Parallax, ParallaxLayer } from '@react-spring/parallax';
 import Banner from 'components/banner/Banner';
 import Profile from 'components/pages-content/profile/Profile';
 import Graduation from 'components/pages-content/graduation/Graduation';
@@ -10,7 +11,8 @@ import Footer from 'components/footer/Footer';
 import { prefix } from 'utils/prefix';
 
 export default function Home() {
-  console.log(prefix)
+  const parallaxRef = useRef<IParallax>();
+
   return (
     <>
       <Head>
@@ -18,13 +20,38 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
+        <Parallax
+          ref={parallaxRef}
+          pages={9}
+          style={{
+            backgroundImage: `url(${prefix}/assets/picture04.jpg)`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          <ParallaxLayer offset={0} speed={0} />
+          <ParallaxLayer offset={0} speed={1}>
             <Banner />
+          </ParallaxLayer>
+          <ParallaxLayer offset={1} speed={0.3}>
             <Profile />
+          </ParallaxLayer>
+          <ParallaxLayer offset={2.18} speed={0.3}>
             <Graduation />
+          </ParallaxLayer>
+          <ParallaxLayer offset={3} speed={0.3}>
             <WorkExp />
+          </ParallaxLayer>
+          <ParallaxLayer offset={4.3} speed={0.3}>
             <Skills />
+          </ParallaxLayer>
+          <ParallaxLayer offset={6.4} speed={0.3}>
             <Projects />
+          </ParallaxLayer>
+          <ParallaxLayer offset={8.5} speed={0.5}>
             <Footer />
+          </ParallaxLayer>
+        </Parallax>
       </div>
     </>
   );
